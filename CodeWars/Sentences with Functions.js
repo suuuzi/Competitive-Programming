0846f140a3c016724ad338c686d21c259553817b
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-07-14 12:45:50 -0300

    Create Sentences with Functions.js

---
+++ CodeWars/Sentences with Functions.js
@@ -0,0 +1,21 @@
+// Problem: Sentences with Functions (http://www.codewars.com/kata/52567c56cb6ac86b56000516)
+
+function Adam(str) { 
+	if (!str || !str.length ){
+		str = "";
+	}
+	return "Adam" + str + "." ; }
+function has(str) { return " has" + str; }
+function a(str) { return " a " + str; }
+function dog(str) { 
+	if (!str || !str.length ){
+		str = "";
+	}	
+	return "dog" + str; 
+	}
+function The(str) { return "The " + str; }
+function name(str) { return "name " + str; }
+function of(str) { return "of " + str;}
+function the(str) { return "the " + str; }
+function is(str) { return " is " + str;}
+function also(str) { return "also " + str; }
