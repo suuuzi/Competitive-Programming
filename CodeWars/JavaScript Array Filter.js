fac16c8e5a6709b8c9602e62b6039aae704552ff
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-07-14 12:40:45 -0300

    Create JavaScript Array Filter.js

---
+++ CodeWars/JavaScript Array Filter.js
@@ -0,0 +1,5 @@
+// Problem: JavaScript Array Filter (http://www.codewars.com/kata/514a6336889283a3d2000001)
+
+function getEvenNumbers(numbersArray){
+  return numbersArray.filter( function y(x){ return x % 2 == 0; })
+}
