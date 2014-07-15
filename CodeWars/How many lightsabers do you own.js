9530997447adafd48113f03255ebe8114294c423
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-07-15 14:29:35 -0300

    Create How many lightsabers do you own.js

---
+++ CodeWars/How many lightsabers do you own.js
@@ -0,0 +1,5 @@
+// Problem: How many lightsabers do you own? (http://www.codewars.com/kata/51f9d93b4095e0a7200001b8)
+
+function howManyLightsabersDoYouOwn(name){
+    return name === "Zach" ? 18 : 0;
+}
