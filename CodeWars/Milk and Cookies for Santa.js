ccc4f11bdbdf57387ddc72ef7b895b5d61662386
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-07-15 14:26:48 -0300

    Create Milk and Cookies for Santa.js

---
+++ CodeWars/Milk and Cookies for Santa.js
@@ -0,0 +1,5 @@
+// Problem: Milk and Cookies for Santa (http://www.codewars.com/kata/52af7bf41f5a1291a6000025)
+
+function timeForMilkAndCookies(date){
+  return date.getDate() === 24 && date.getMonth() === 11;
+}
