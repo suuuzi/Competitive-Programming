3df1d59f59a24544e4b27b472aebbb23c22243dd
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-07-23 10:04:53 -0300

    Create 1004 - Produto Simples.c

---
+++ URI/1004 - Produto Simples.c
@@ -0,0 +1,10 @@
+// Problem: 1004 - Produto Simples (https://www.urionlinejudge.com.br/judge/pt/problems/view/1004)
+
+#include "stdio.h"
+int main()
+{
+    int a = 0, b = 0;   
+    scanf("%d%d", &a, &b);  
+    printf("PROD = %d\n", a * b);
+    return 0;
+}
