686ce2b9ce44a2c6c5a6f284d31fc979d95f2b48
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-07-23 09:47:16 -0300

    Create Basic JS - Calculating averages.js

---
+++ CodeWars/Basic JS - Calculating averages.js
@@ -0,0 +1,11 @@
+// Problem: Basic JS - Calculating averages (http://www.codewars.com/kata/529f32794a6db5d32a00071f)
+
+var Calculator = {
+ average: function() {
+  var s = 0;
+  for(var i = 0; i < arguments.length; i++){
+    s += parseInt(arguments[i]);
+  }
+  return arguments.length === 0 ? 0 : s / arguments.length;
+ }
+};
