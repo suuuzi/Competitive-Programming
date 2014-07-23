4bb86eb576af1b52876d6ed6d85051d1f58afefe
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-07-23 10:04:01 -0300

    Create 1003 - Soma Simples.c

---
+++ URI/1003 - Soma Simples.c
@@ -0,0 +1,10 @@
+// Problem: 1003 - Soma Simples (https://www.urionlinejudge.com.br/judge/pt/problems/view/1003)
+
+#include "stdio.h"
+int main()
+{
+    int a = 0, b = 0;   
+    scanf("%d%d", &a, &b);  
+    printf("SOMA = %d\n", a + b);
+    return 0;
+}
