97f41a4148d59403c809267be21ca0fca6c7e3d1
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-07-23 10:02:07 -0300

    Create 1001 - Extremamente Básico.c

---
+++ URI/1001 - Extremamente Básico.c
@@ -0,0 +1,11 @@
+// Problem: 1001 - Extremamente Básico (https://www.urionlinejudge.com.br/judge/pt/problems/view/1001)
+
+#include "stdio.h"
+ 
+int main()
+{
+    int a = 0, b = 0;
+    scanf("%d%d", &a, &b);
+    printf("X = %d\n", a + b);
+    return 0;
+}
