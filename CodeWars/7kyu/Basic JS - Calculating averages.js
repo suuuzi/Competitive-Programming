97b9f99ae17d1703502fe1c94ee2846492ae1bd0
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-07-31 13:36:23 -0300

    Update and rename CodeWars/Basic JS - Calculating averages.js to CodeWars/7kyu/Basic JS - Calculating averages.js

--- CodeWars/7kyu/Basic JS - Calculating averages.js
+++ CodeWars/7kyu/Basic JS - Calculating averages.js
@@ -3,9 +3,11 @@
 var Calculator = {
  average: function() {
   var s = 0;
+  
   for(var i = 0; i < arguments.length; i++){
     s += parseInt(arguments[i]);
   }
+  
   return arguments.length === 0 ? 0 : s / arguments.length;
  }
 };
