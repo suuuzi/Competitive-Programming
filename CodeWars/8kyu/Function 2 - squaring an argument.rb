057a44d669973c12068cbe380bfd87a6df5cfc0d
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-08-01 14:47:57 -0300

    fixing comment (duh)

--- CodeWars/8kyu/Function 2 - squaring an argument.rb
+++ CodeWars/8kyu/Function 2 - squaring an argument.rb
@@ -1,4 +1,4 @@
-// Problem: Function 2 - squaring an argument (http://www.codewars.com/kata/function-2-squaring-an-argument/ruby)
+# Problem: Function 2 - squaring an argument (http://www.codewars.com/kata/function-2-squaring-an-argument/ruby)
 
 def square(var)
    return var * var
