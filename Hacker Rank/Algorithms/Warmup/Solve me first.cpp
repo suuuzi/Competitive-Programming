3278d513f512c2628dcc98feca906d77f6cc70c6
suuuzi <tetestonaldo@gmail.com>
2014-11-15 16:50:19 -0200

    adicionando exercício teste do hacker rank

---
+++ Hacker Rank/Algorithms/Warmup/Solve me first.cpp
@@ -0,0 +1,13 @@
+// Problem: Solve me first (https://www.hackerrank.com/challenges/solve-me-first)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 0, b = 0;
+  
+  cin >> a >> b;
+  cout << a + b << endl;
+  
+  return 0;
+}
