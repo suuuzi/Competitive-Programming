509e5b0364a769290a62b583b698656fa6108cd3
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-12-06 11:52:19 -0200

    Create 1059 - Números Pares.c

---
+++ URI/1. Iniciante/1059 - Números Pares.c
@@ -0,0 +1,14 @@
+// Problem: 1059 - Números Pares (https://www.urionlinejudge.com.br/judge/pt/problems/view/1059)
+
+#include "stdio.h"
+ 
+int main()
+{
+    for(int i = 1; i <= 100; i++)
+    {
+        if(i % 2 == 0)
+            printf("%d\n", i);
+    }
+     
+    return 0;
+}
