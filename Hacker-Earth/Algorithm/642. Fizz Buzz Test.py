23d6f4d1202f23344e08e2cf5be4ec07a6e98488
Suzane Sant Ana <tetestonaldo@gmail.com>
2014-12-11 00:17:31 -0200

    Create 642. Fizz Buzz Test.py

---
+++ Hacker-Earth/Algorithm/642. Fizz Buzz Test.py
@@ -0,0 +1,12 @@
+# Problem: 642. Fizz Buzz Test (http://www.hackerearth.com/problem/algorithm/fizzbuzz/)
+
+if i % 3 == 0 or i:
+	if i % 5 == 0:
+		print("FizzBuzz")
+	print("Fizz")
+	
+elif i % 5 == 0:
+	print("Buzz")
+	
+else:
+	print str(i)
