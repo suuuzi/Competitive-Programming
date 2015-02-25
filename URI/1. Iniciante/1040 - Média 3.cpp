e98e8982e732278f252bda17f6686d9c6550a23c
suuuzi <tetestonaldo@gmail.com>
2015-02-25 20:54:39 -0300

    correção: mudança de escopo do exercício

--- URI/1. Iniciante/1040 - Média 3.cpp
+++ URI/1. Iniciante/1040 - Média 3.cpp
@@ -1,39 +1,38 @@
-// Problem: 1040 - Média 3 (https://www.urionlinejudge.com.br/judge/pt/problems/view/1040)
-
 #include <iostream>
 #include <iomanip>
 using namespace std;
- 
+
 int main() {
- 
+
     double n1 = 0, n2 = 0, n3 = 0, n4 = 0, exame = 0;
     cin >> n1 >> n2 >> n3 >> n4;
-     
+
     double media = (2 * n1 + 3 * n2 + 4 * n3 + n4) / 10;
     cout << fixed << setprecision(1);
-     
+
     cout << "Media: " << media << endl;
-     
+
     if (media >= 7.0)
         cout << "Aluno aprovado." << endl;
-    else    
+    else
         if (media >= 5.0){
             cout << "Aluno em exame." << endl;
             cin >> exame;
-             
+
             cout << "Nota do exame: " << exame << endl;
             media = (media + exame) / 2;
-             
+
             if (media >= 5.0)
                 cout << "Aluno aprovado." << endl;
             else
                 cout << "Aluno reprovado." << endl;
-        }       
+        }
         else {
             cout << "Aluno reprovado." << endl;
         }
-         
-        cout << "Media final: " << media << endl;
- 
+
+        if(exame > 0)
+          cout << "Media final: " << media << endl;
+
     return 0;
 }
