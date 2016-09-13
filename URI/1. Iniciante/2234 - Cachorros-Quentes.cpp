9a9199f63dc787fcd7d436d098d59784039d31c3
Suzane Sant Ana <tetestonaldo@gmail.com>
2016-09-12 22:02:47 -0300

    Create 2234 - Cachorros-Quentes.cpp

---
+++ URI/1. Iniciante/2234 - Cachorros-Quentes.cpp
@@ -0,0 +1,14 @@
+// 2234 - Cachorros-Quentes (https://www.urionlinejudge.com.br/judge/pt/problems/view/2234)
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+	double n = 0, p = 0;
+	cin >> n >> p;
+	cout << fixed << setprecision(2) << (n / p) << endl;
+	
+	return 0;
+}
