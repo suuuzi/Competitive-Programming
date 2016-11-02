3c0eee7da10a8ee6e929f23487968d63a594e159
Suzane Sant Ana <tetestonaldo@gmail.com>
2016-11-02 16:18:30 -0200

    Create How do I compare numbers.py

---
+++ CodeWars/8kyu/How do I compare numbers.py
@@ -0,0 +1,8 @@
+# How do I compare numbers (https://www.codewars.com/kata/how-do-i-compare-numbers)
+def what_is(x):
+    if x is 42:
+        return 'everything'
+    elif x == 42 * 42:
+        return 'everything squared'
+    else:
+        return 'nothing'
