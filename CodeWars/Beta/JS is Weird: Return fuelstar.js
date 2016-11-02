291c851def8edf53c29bdddd220b753c5739c654
Suzane Sant Ana <tetestonaldo@gmail.com>
2016-11-02 17:20:04 -0200

    Create JS is Weird: Return fuelstar.js

---
+++ CodeWars/Beta/JS is Weird: Return fuelstar.js
@@ -0,0 +1,2 @@
+// JS is Weird: Return fuelstar (https://www.codewars.com/kata/js-is-weird-return-fuelstar/javascript)
+x=(!!x+[!x]+x)[+!!x]+(!x+[!x]+x)[(+!!x)+(+!x)+(+!x)]+(!x+[!x]+x)[(+!!x)+(+!x)+(+!x)+(+!x)]+(!!x+[!x]+x)[+!!x+(+!x)+(+!x)]+(!!x+[!x]+x)[+!!x+(+!x)+(+!x)+(+!x)]+(!x+[!x]+x)[(+!!x)]+(!!x+[!x]+x)[+!!x+(+!x)]+(!x+[!x]+x)[(+!!x)+(+!x)]
