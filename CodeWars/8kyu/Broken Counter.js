f8e2a82202d34bfc6247c8ca66316abe49177169
Suzane Sant Ana <tetestonaldo@gmail.com>
2016-11-05 20:39:18 -0200

    Create Broken Counter.js

---
+++ CodeWars/8kyu/Broken Counter.js
@@ -0,0 +1,17 @@
+// Broken Counter (https://www.codewars.com/kata/broken-counter)
+
+function Counter() {
+  this.value = 0;
+}
+
+Counter.prototype.increase = function() {
+  this.value++;
+};
+
+Counter.prototype.getValue = function() {
+  return this.value;
+};
+
+Counter.prototype.reset = function() {
+  this.value = 0;
+};
